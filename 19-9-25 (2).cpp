d66f0b3cabcccf0f7f716801563af020211abdfb
vijju8161 <vijjuvijju5641@gmail.com>
2025-12-05 15:35:16 +0530

    Add files via upload

---
+++ 19-9-25 (2).cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{  
+   float a,b;
+   printf("enter one number");
+   scanf("%f",&a);
+   b=sqrt(a);
+   printf("%f",b);
+   
+   return 0;
+}
+	
+
